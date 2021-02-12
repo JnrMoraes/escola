97c1265df204fea7a53dd1c0f949dadbcce1c2a1
juniormoraes <contatomoraesjr@gmail.com>
2021-02-12 11:16:43 -0300

    Materia controller

--- src/main/java/com/aluno/alunosmateriasnotas/controller/MateriaController.java
+++ src/main/java/com/aluno/alunosmateriasnotas/controller/MateriaController.java
@@ -3,10 +3,14 @@ package com.aluno.alunosmateriasnotas.controller;
 import com.aluno.alunosmateriasnotas.model.entities.Materia;
 import com.aluno.alunosmateriasnotas.repositories.MateriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/materia")
@@ -15,9 +19,33 @@ public class MateriaController {
     @Autowired
     private MateriaRepository materiaRepository;
 
-    @PostMapping("/nova")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Materia> getMateria(@PathVariable Long id){
+        return materiaRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public Iterable<Materia> getMaterias(){
+        return materiaRepository.findAll();
+    }
+
+    @RequestMapping(value = "/nova", method = RequestMethod.POST)
     public @ResponseBody Materia novaMateria(Materia materia){
         materiaRepository.save(materia);
         return materia;
     }
+
+    @RequestMapping(value = "/alterar", method = RequestMethod.PUT)
+    public @ResponseBody Materia updateMateria(@RequestBody Materia materia){
+        materiaRepository.save(materia);
+        return materia;
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public void deleteMateria(@PathVariable Long id){
+        materiaRepository.deleteById(id);
+    }
+
 }
+
+
