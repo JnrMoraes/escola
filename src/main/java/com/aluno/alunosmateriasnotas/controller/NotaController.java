e7c4fbbf4bd653ccf4d42146f0c6d61f85a124ec
juniormoraes <contatomoraesjr@gmail.com>
2021-02-18 16:30:46 -0300

    Adicionando metodo alterar em nota controller

--- src/main/java/com/aluno/alunosmateriasnotas/controller/NotaController.java
+++ src/main/java/com/aluno/alunosmateriasnotas/controller/NotaController.java
@@ -61,8 +61,24 @@ public class NotaController {
     }
 
     @PutMapping
-    public ResponseEntity<Void> alterarNota() {
-        throw new NotYetImplementedException();
+    public ResponseEntity<Boolean> alterarNota(@RequestBody Nota nota) {
+        try {
+            Nota notaAtualizada = this.notaRepository.findById(nota.getId()).get();
+            notaAtualizada.setBimetre1(nota.getBimetre1());
+            notaAtualizada.setBimetre2(nota.getBimetre2());
+            notaAtualizada.setBimetre3(nota.getBimetre3());
+            notaAtualizada.setBimetre4(nota.getBimetre4());
+            notaAtualizada.setMediaBimentre1(nota.getMediaBimentre1());
+            notaAtualizada.setMediaBimentre2(nota.getMediaBimentre2());
+            notaAtualizada.setMediaBimentre3(nota.getMediaBimentre3());
+            notaAtualizada.setMediaBimentre4(nota.getMediaBimentre4());
+            notaAtualizada.setMediaFinal(nota.getMediaFinal());
+            this.notaRepository.save(notaAtualizada);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(true);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @DeleteMapping("/{id}")
