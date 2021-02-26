32c6b2464c4e42db4637cf97865b8d08ea5c6f0e
juniormoraes <contatomoraesjr@gmail.com>
2021-02-25 22:05:46 -0300

    teste unitario para cadatrarAluno, usado Junit esta quebrando, corrigir

--- src/test/java/com/aluno/alunosmateriasnotas/controller/AlunoControllerUnitTest.java
+++ src/test/java/com/aluno/alunosmateriasnotas/controller/AlunoControllerUnitTest.java
@@ -1,6 +1,9 @@
 package com.aluno.alunosmateriasnotas.controller;
 
+
 import com.aluno.alunosmateriasnotas.dto.AlunoDto;
+import com.aluno.alunosmateriasnotas.dto.MateriaDto;
+import com.aluno.alunosmateriasnotas.entity.Materia;
 import com.aluno.alunosmateriasnotas.model.Response;
 import com.aluno.alunosmateriasnotas.service.IAlunoService;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,6 +19,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
@@ -40,7 +44,47 @@ class AlunoControllerUnitTest {
     private TestRestTemplate restTemplate;
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
+
+        MateriaDto materiaDto = new MateriaDto();
+        materiaDto.setId(2L);
+        materiaDto.setNome("Ciencias");
+
+        Materia materia1 = new Materia();
+        materia1.setNome("Portugues");
+
+        Materia materia2 = new Materia();
+        materia2.setNome("Matemática");
+
+        List<Materia> materias = new ArrayList<>();
+        materias.add(materia1);
+        materias.add(materia2);
+
+        AlunoDto alunoDto = new AlunoDto();
+        alunoDto.setId(1L);
+        alunoDto.setNome("Jose de Jesus");
+        alunoDto.setMaterias(materias);
+
+    }
+
+
+    @Test
+    void testCadastrarAluno() {
+        AlunoDto alunoDto = new AlunoDto();
+        alunoDto.setId(1L);
+        alunoDto.setNome("Jose de Jesus");
+
+        Mockito.when(this.alunoService.cadastrarAluno(alunoDto))
+                .thenReturn(Boolean.TRUE);
+
+        HttpEntity<AlunoDto> request = new HttpEntity<>(alunoDto);
+
+        ResponseEntity<Response<Boolean>> alunoCriado = restTemplate
+                .exchange("http://localhost:" + this.port + "/aluno",HttpMethod.POST, request,
+                        new ParameterizedTypeReference<Response<Boolean>>(){
+                        });
+        assertNotNull(alunoCriado.getBody().getData());
+        assertEquals(201, alunoCriado.getBody().getStatusCode());
     }
 
     @Test
@@ -49,11 +93,20 @@ class AlunoControllerUnitTest {
         Mockito.when(this.alunoService.consultarAlunos())
                 .thenReturn(new ArrayList<AlunoDto>());
         ResponseEntity<Response<List<AlunoDto>>> alunos = restTemplate
-                .exchange("http://localhost:"+this.port+"/aluno", HttpMethod.GET, null,
-                       new ParameterizedTypeReference<Response<List<AlunoDto>>>() {
-        });
+                .exchange("http://localhost:" + this.port + "/aluno", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<Response<List<AlunoDto>>>() {
+                        });
         assertNotNull(alunos.getBody().getData());
         assertEquals(200, alunos.getBody().getStatusCode());
 
     }
+
+    @Test
+    void alterarAluno() {
+    }
+
+    @Test
+    void deletarAluno() {
+    }
+
 }
