8c4766e17a546c2ade1b78b79e7d5845694abaea
juniormoraes <contatomoraesjr@gmail.com>
2021-03-01 21:39:34 -0300

    Testando a service com de sucesso para listarTodosAlunos, falta testae exceptions

--- src/test/java/com/aluno/alunosmateriasnotas/service/AlunoServiceUnitTest.java
+++ src/test/java/com/aluno/alunosmateriasnotas/service/AlunoServiceUnitTest.java
@@ -13,18 +13,22 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
-class AlunoServiceTest {
+class AlunoServiceUnitTest {
 
     @Mock
     private IAlunoRepository alunoRepository;
@@ -39,33 +43,46 @@ class AlunoServiceTest {
 
     @BeforeAll
     public static void init(){
-        nota = new Nota();
-        nota.setBimetre1(new double[]{7.9, 7.7, 9.8, 6.9});
-        nota.setMediaBimentre1(7.9);
-
-
-        materia = new Materia();
-        materia.setId(2L);
-        materia.setNome("Portugues");
-        materia.setNota(nota);
-//
-//        List<Materia> materias = new ArrayList<>();
-//        materias.add(materia);
 
         aluno = new Aluno();
         aluno.setId(1L);
         aluno.setNome("Jose de Jesus");
-//        alunoDto.setMaterias(materias);
-    }
 
+    }
 
     @Test
     void cadastrarAluno() {
+        AlunoDto alunoDto = new AlunoDto();
+        alunoDto.setNome("Jose de Jesus");
+
+        aluno.setId(null);
+
+        Mockito.when(this.alunoRepository.save(aluno)).thenReturn(Optional.of(aluno).get());
+
+        Boolean sucesso = this.alunoService.cadastrarAluno(alunoDto);
+
+        assertTrue(sucesso);
+        Mockito.verify(this.alunoRepository,times(1)).save(aluno);
+        aluno.setId(1L);
 
     }
 
     @Test
     void alterarAluno() {
+        AlunoDto alunoDto = new AlunoDto();
+        alunoDto.setId(1L);
+        alunoDto.setNome("Jose de Jesus");
+
+        Mockito.when(this.alunoRepository.findById(1L)).thenReturn( Optional.of(aluno));
+        Mockito.when(this.alunoRepository.save(aluno)).thenReturn(aluno);
+
+        Boolean sucesso = this.alunoService.alterarAluno(alunoDto);
+
+        assertTrue(sucesso);
+
+        Mockito.verify(this.alunoRepository,times(1)).findById(1L);
+        Mockito.verify(this.alunoRepository,times(1)).save(aluno);
+
     }
 
     @Test
@@ -88,10 +105,25 @@ class AlunoServiceTest {
 
     @Test
     void consultarAlunoPeloId() {
+        Mockito.when(this.alunoRepository.findById(1L)).thenReturn( Optional.of(aluno));
+        AlunoDto alunoDto = this.alunoService.consultarAlunoPeloId(1L);
+
+        assertNotNull(alunoDto);
+        assertEquals("Jose de Jesus", alunoDto.getNome());
+        assertEquals(1, alunoDto.getId());
+
+        Mockito.verify(this.alunoRepository, times(1)).findById(1L);
+
     }
 
     @Test
     void excluirAluno() {
+        Mockito.when(this.alunoRepository.findById(1L)).thenReturn( Optional.of(aluno));
+        Boolean sucesso = this.alunoService.excluirAluno(1L);
+
+        Mockito.verify(this.alunoRepository,times(1)).findById(1L);
+        Mockito.verify(this.alunoRepository,times(1)).deleteById(1L);
+
     }
 }
 
